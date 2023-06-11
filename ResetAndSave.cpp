2e4e093b4a65c64a830cbba2f4200777a15419a8
Gustavo-Brian-Maciel-Rodrigues-Portela <GustavoBrianGithub@gmail.com>
2023-06-11 11:40:53 -0300

    ok

---
+++ ResetAndSave.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+
+    system("START  ../inicializarTroca.exe");
+    system("START salvar.exe");
+
+    return 0;
+}
--- salvar.cpp
+++ salvar.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main() {
 
-    system("START ../inicializarTroca.exe");
     system("git add .");
     system("git commit -m ok");
     system("git push -u origin master");
