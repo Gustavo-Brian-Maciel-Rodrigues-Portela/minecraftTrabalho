4ab168c21705bcd4b89a3b502fdf6e7e1cc060dd
Gustavo-Brian-Maciel-Rodrigues-Portela <GustavoBrianGithub@gmail.com>
2023-06-11 10:41:08 -0300

    ok

--- clone.cpp
+++ clone.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-    system("git clone https://github.com/Gustavo-Brian-Maciel-Rodrigues-Portela/minecraftTrabalho ..");
+    system("git clone https://github.com/Gustavo-Brian-Maciel-Rodrigues-Portela/minecraftTrabalho ../minecraftTrabalho");
 
     return 0;
 }
